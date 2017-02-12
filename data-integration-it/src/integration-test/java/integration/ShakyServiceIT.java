8f3556e48e1fb6df40a512836242c9044d567439
Josh Long <josh@joshlong.com>
2017-02-11 21:47:31 -0800

    removing the empty test for the moment.

--- data-integration-it/src/integration-test/java/integration/ShakyServiceIT.java
+++ data-integration-it/src/integration-test/java/integration/ShakyServiceIT.java
@@ -1,5 +1,6 @@
 package integration;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 /**
  * @author <a href="mailto:josh@joshlong.com">Josh Long</a>
  */
+@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = ShakyServiceIT.Config.class)
 public class ShakyServiceIT {
