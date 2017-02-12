a79340a9d37c552a5686ba662fa837733267f568
Josh Long <josh@joshlong.com>
2017-02-12 03:47:08 -0800

    typo. i walked right into that. i should've used a configuration properties object!

--- data-integration-it/src/integration-test/java/integration/ShakyServiceIT.java
+++ data-integration-it/src/integration-test/java/integration/ShakyServiceIT.java
@@ -20,7 +20,6 @@ import org.springframework.web.client.RestTemplate;
 import java.io.File;
 import java.util.stream.Stream;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = ShakyServiceIT.Config.class)
 public class ShakyServiceIT {
