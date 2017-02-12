e6159316a7e035d3badb302450997903bd926731
Josh Long <josh@joshlong.com>
2017-02-11 22:13:44 -0800

    trying to get the shaky service and client deployed.

--- data-integration-it/src/integration-test/java/integration/ShakyServiceIT.java
+++ data-integration-it/src/integration-test/java/integration/ShakyServiceIT.java
@@ -1,10 +1,16 @@
 package integration;
 
+import cnj.CloudFoundryService;
+import org.junit.Before;
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.File;
 
 @Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -12,8 +18,32 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class ShakyServiceIT {
 
 	@SpringBootApplication
-	public static class Config {}
+	public static class Config {
+	}
 
+	private File shakyClient, shakyService;
+
+	@Autowired
+	private CloudFoundryService service;
+
+	@Before
+	public void before() throws Throwable {
+		File project = new File(".");
+		this.shakyClient = new File(project, "../shaky-service-to-service-calls/shaky-client");
+		this.shakyService = new File(project, "../shaky-service-to-service-calls/shaky-service");
+
+		this.service.pushApplicationAndCreateUserDefinedServiceUsingManifest(
+				this.shakyService);
+
+		this.service.pushApplicationUsingManifest(
+				this.shakyClient);
+
+	}
+
+	@Test
+	public void deployServiceAndClient() throws Throwable {
+
+	}
 
 
 }
