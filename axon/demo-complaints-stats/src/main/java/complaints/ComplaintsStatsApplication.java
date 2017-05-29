bffc38af16b18c40dfa5b79859c42af0b53ad103
Josh Long <josh@joshlong.com>
2017-05-28 20:17:44 -0700

    cd

--- axon/demo-complaints-stats/src/main/java/complaints/ComplaintsStatsApplication.java
+++ axon/demo-complaints-stats/src/main/java/complaints/ComplaintsStatsApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class ComplaintsStatsApplication {
 
- 
+
  public static void main(String[] args) {
   SpringApplication.run(ComplaintsStatsApplication.class, args);
  }
